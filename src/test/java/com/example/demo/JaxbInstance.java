34dc2f308ef3c850abb6837729773db5f70c2018
cj96248 <253037585@qq.com>
2018-09-03 22:51:48 +0800

    Add jaxb.index

--- src/test/java/com/example/demo/JaxbInstance.java
+++ src/test/java/com/example/demo/JaxbInstance.java
@@ -2,27 +2,31 @@ package com.example.demo;
 
 import javax.xml.bind.JAXBContext;
 
+import org.junit.Test;
+
 import com.example.bean.Teacher;
 
 public class JaxbInstance {
-	
+	@Test
 	public void simpleClass() throws Exception {
 		JAXBContext instance = JAXBContext.newInstance(Student.class);
 		System.out.println(instance);
 	}
-	
+	@Test
 	public void multiClass() throws Exception {
 		JAXBContext instance = JAXBContext.newInstance(Student.class, Teacher.class);
 		System.out.println(instance);
 	}
 
+	@Test
 	public void simplePackage() throws Exception {
 		JAXBContext instance = JAXBContext.newInstance("com.example.bean");
 		System.out.println(instance);
 	}
-	
+	@Test
 	public void multiPackage() throws Exception {
 		JAXBContext instance = JAXBContext.newInstance("com.example.bean:com.example.pojo");
 		System.out.println(instance);
 	}
+	
 }
